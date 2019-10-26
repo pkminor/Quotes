ef909956df56df4a8d1903f3b535c6ef2b01982f
minorpk <minorpk@gmail.com>
2019-10-26 09:51:03 +0300

    added input controls to component quote-form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  @Output() addQuote = new EventEmitter<Quote>();
+  newQuote = new Quote("","","");
   constructor() { }
 
   ngOnInit() {
   }
+
+  submitQuote(){ this.addQuote.emit(this.newQuote); }
 
 }
