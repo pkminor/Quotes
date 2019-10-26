7ba25fdd8b11a519de082b4f282b9c527846088a
minorpk <minorpk@gmail.com>
2019-10-26 23:58:38 +0300

    completed pipe timeline

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[]=[
-    new Quote(1,"Barack Obama","username", "Yes we can.",new Date()),
-    new Quote(2,"Lupita Nyong'o","username", "Dreams are valid.",new Date()),
+    new Quote(1,"Barack Obama","username", "Yes we can.",new Date("2019-08-08")),
+    new Quote(2,"Lupita Nyong'o","username", "Dreams are valid.",new Date("2019-10-26")),
     new Quote(3,"Eliud Kipchoge","username", "No human is limited.",new Date()),
     new Quote(4,"Albert Einstein","username", "Everything should be made as simple as possible, but not simpler.",new Date())
    ];
--- src/app/timeline.pipe.ts
+++ src/app/timeline.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimelinePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): string {
+
+    let today:Date=new Date();
+    let todayWithNoTime:any=new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference = Math.abs(value-today);
+    const secondsInDay=86400;
+    var dateDifferenceSeconds=dateDifference*0.001;
+
+    if(dateDifferenceSeconds<60) return ` Just now`;
+    else if(dateDifferenceSeconds<3600) return `${Math.floor(dateDifferenceSeconds/60)} Minutes ago`;
+    else if(dateDifferenceSeconds<86400) return `${Math.floor(dateDifferenceSeconds/3600)}  Hours ago` ;
+    else return `${Math.floor(dateDifferenceSeconds/86400)} Days ago`;
+
   }
 
 }
