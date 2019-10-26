aeef03fbc8f4b1c6d83a28e768ceabd3dbcc627e
minorpk <minorpk@gmail.com>
2019-10-26 08:42:52 +0300

    added quote property in component quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[]=[
+    new Quote("Albert Einstein","username",
+    "Everything should be made as simple as possible, but not simpler")
+   ];
 
   constructor() { }
 
