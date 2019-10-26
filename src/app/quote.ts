894adb1153003e3ce70d9162203d0d162c6f6eb1
minorpk <minorpk@gmail.com>
2019-10-26 08:50:03 +0300

    added component quote-form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,8 +2,8 @@ export class Quote {
 
   createDate:Date; //new Date() gives back a datetime.
   likes:number;
-  dislike:number;
-  
+  dislikes:number;
+
   constructor
   (public author:string,public user:string,public content:string)
   {this.createDate=new Date(); this.likes=0; this.dislikes=0;}
