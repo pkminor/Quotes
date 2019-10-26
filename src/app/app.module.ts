2837418aa4ce9b9801671eb91b58cb81b2aab184
minorpk <minorpk@gmail.com>
2019-10-26 18:19:56 +0300

    changed component quote-form position to top right

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteVoteComponent
   ],
   imports: [
     BrowserModule,
